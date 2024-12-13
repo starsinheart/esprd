eef9122aa2ac70dd0d23f30c8f1d7dda2cd617cd
starsinheart <o.imazhap@sibstrin.ru>
2024-12-14 02:43:48 +0700

    again

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/post_data/', views.post_data, name='post_data'),
-    path('blog/get_data/', views.get_data, name='get_data'),
+    path('post_data/', views.post_data, name='post_data'),
+    path('get_data/', views.get_data, name='get_data'),
 ]
