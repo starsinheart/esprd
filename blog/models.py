baa40916c62474474473560dacfd1cfbf9bfa936
starsinheart <o.imazhap@sibstrin.ru>
2024-12-21 09:43:05 +0700

    interaction

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 
-class IncrementValue(models.Model):
-    value = models.IntegerField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-class Command(models.Model):
-    command = models.CharField(max_length=100)
-    executed = models.BooleanField(default=False)
+class SensorData(models.Model):
+    sensor_value = models.FloatField()
     timestamp = models.DateTimeField(auto_now_add=True)
